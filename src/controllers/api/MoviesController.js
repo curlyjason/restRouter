cecac6d45424595acfb069fcf03c26625d777cef
dreamingmind <ddrake@dreamingmind.com>
2023-04-20 16:31:16 -0700

    add a Movies controller/table

---
+++ src/controllers/api/MoviesController.js
@@ -0,0 +1,52 @@
+class MoviesController
+{
+
+    Movies = require('../../model/GenresTable');
+
+    /**
+     * @returns {[{name: string, id: number},{name: string, id: number}]}
+     */
+    async index() {
+        console.log('waiting...');
+        return await this.Movies.find();
+    }
+
+    /**
+     * @param id
+     * @returns {{name: string, id: number}|{name: string, id: number}} | undefined
+     */
+    async view(id) {
+        return await this.Movies.findById(id);
+    }
+
+    /**
+     * @param {{name}} data
+     * @returns {{name, id: *}}
+     */
+    async add(data) {
+        let { error } = this.Movies.joiSchema.validate(data);
+        if(error) return {
+            error: error.message,
+            post_data: data
+        };
+
+        return await this.Movies.save(data);
+    }
+
+    async edit(id, data) {
+        let genre = await this.Movies.findById(id);
+        return await this.Movies.update(genre, data);
+    }
+
+    async delete(id) {
+        let genre = await this.Movies.findById(id);
+        if(genre){
+            return await this.Movies.delete(genre);
+        }
+        else {
+            return new Error('Record not found');
+        }
+    }
+}
+
+module.exports = new MoviesController();
---
+++ src/model/MoviesTable.js
@@ -0,0 +1,67 @@
+
+class MoviesTable extends require('./Table.js') {
+
+    joiSchema = this.Joi.object({
+        name: this.Joi.string()
+            .pattern(/^[a-zA-Z_ -]{3,100}$/)
+            .required(),
+    });
+
+    schema = new this.mongoose.Schema({
+        name: String,
+    })
+
+    Movies = this.mongoose.model('genres', this.schema);
+
+    async find () {
+        let find = async () => {
+            return await this.Movies.find()
+                .then((result) => {
+                    return result;
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                    return err;
+                })
+        };
+        return await this.connection(find)
+    }
+
+    async findById (id) {
+        let findById = async () => {
+            return await this.Movies.findById(id)
+                .then((result) => {
+                    return result;
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                    return err;
+                });
+        }
+        return await this.connection(findById);
+    }
+
+    async save (data) {
+        let save = async () => {
+            let entity = new this.Movies(data);
+            return await entity.save();
+        }
+        return await this.connection(save);
+    }
+
+    async update (entity, data) {
+        let update = async () => {
+            return await entity.updateOne(data);
+        }
+        return await this.connection(update);
+    }
+
+    async delete (entity) {
+        let update = async () => {
+            return await entity.deleteOne();
+        }
+        return await this.connection(update);
+    }
+}
+
+module.exports = new MoviesTable();
