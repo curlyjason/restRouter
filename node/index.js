53d6d4a111a42096016edf5c7f0685d14e588c1d
dreamingmind <ddrake@dreamingmind.com>
2023-04-21 13:50:57 -0700

    start adding a second router

--- node/index.js
+++ node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const api_routes = require('./src/routes/ApiRoutes');
+const mvc_routes = require('./src/routes/MVCRoutes');
 
 const app = express();
 
@@ -9,6 +10,7 @@ const HOST = '0.0.0.0';
 
 app.use(express.json());
 app.use('/api/', api_routes);
+app.use('/', mvc_routes);
 
 app.listen(PORT, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
--- src/routes/MVCRoutes.js
+++ src/routes/MVCRoutes.js
@@ -2,99 +2,138 @@ const express = require('express');
 const router = express.Router();
 const inflector = require("../utility/Inflect");
 
-function getController(controller, req, res) {
-    try {
-        let className = '../controllers/api/' + inflector.capitalize(controller) + 'Controller';
-        return require(className);
-    } catch (e) {
-        console.log(e);
-        return false;
-    }
-};
+router.get('/:controller/:action', (req, res) => {
+    return res.status(200).send([req.params, req.query, req.pass]);
+})
+router.get('/:controller', (req, res) => {
+    req.params.action = req.params.action ?? 'index';
 
-function contentIsJson(req) {
-    return req.header('content-type') === 'application/json'
-        && req.body instanceof Object;
+    return res.status(200).send([req.params, req.query, req.pass]);
+})
+router.get('/:controller/:action/*', (req, res) => {
+    req.pass = req.params[0].split('/');
+    return res.status(200).send([req.params, req.query, req.pass]);
+})
+
+async function routeHandler(req, res) {
+    req.params.action = req.params.action ?? 'index';
+
+    let controller = locateController(req)
+    if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+
+    let actionExists = validate(controller, req.params.action)
+    if(!actionExists) return res.status(404).send(`${req.params.controller} could not be found`);
+
+    parsePassArgs(req);
+
+
 }
 
-function edit(req, res) {
-    let controller = getController(req.params.controller);
-    if (!controller) return res.status(404).send(`${req.params.controller} could not be found`);
-    if (!contentIsJson(req)) return res.status(400).send("Request body must be 'application/json");
+function parsePassArgs(req) {
 
-    let result = controller.edit(req.params.id, req.body);
-    if (!result) return res.status(404).send(`The requested '${req.params.controller}' record could not be found`);
-
-    return res.status(200).send(result);
 }
 
-/**
- * index
- */
-router.get('/:controller', async (req, res) => {
-    let controller = getController(req.params.controller);
-    if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
-
-    return res.send(await controller.index());
-})
-
-/**
- * view
- */
-router.get('/:controller/:id', async (req, res) => {
-    let controller = getController(req.params.controller);
-    if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
-
-    let result = await controller.view(req.params.id);
-    if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
-
-    return res.status(200).send(result);
-})
-
-/**
- * add
- */
-router.post('/:controller', async (req, res) => {
-    let controller = getController(req.params.controller);
-    if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
-
-    let result = await controller.add(req.body);
-    if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
-
-    return res.status(200).send(result);
-})
-
-async function apiPatchPutHandler(req, res) {
-    let controller = getController(req.params.controller);
-    if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
-
-    let result = await controller.edit(req.params.id, req.body);
-    if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
-
-    return res.status(200).send(result);
-}
-
-/**
- * edit
- */
-router.patch('/:controller/:id', apiPatchPutHandler)
-
-/**
- * edit
- */
-router.put('/:controller/:id',  apiPatchPutHandler)
-
-/**
- * delete
- */
-router.delete('/:controller/:id', (req, res) => {
-    let controller = getController(req.params.controller);
-    if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
-
-    let result = controller.delete(req.params.id);
-    if (!result) return res.status(404).send(`The requested '${req.params.controller}' record could not be found`);
-
-    return res.status(200).send(result);
-})
+// function getController(controller, req, res) {
+//     try {
+//         let className = '../controllers/' + inflector.capitalize(controller) + 'Controller';
+//         return require(className);
+//     } catch (e) {
+//         console.log(e);
+//         return false;
+//     }
+// };
+//
+// function edit(req, res) {
+//     let controller = getController(req.params.controller);
+//     if (!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//
+//     let result = controller.edit(req.params.id, req.body);
+//     if (!result) return res.status(404).send(`The requested '${req.params.controller}' record could not be found`);
+//
+//     return res.status(200).send(result);
+// }
+//
+// /**
+//  * index
+//  */
+// router.get('/:controller', async (req, res) => {
+//     let controller = getController(req.params.controller);
+//     if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//
+//     return res.send(await controller.index());
+// })
+//
+// router.get('/:controller/:action/*', async (req, res) => {
+//     return res.status(200).send(req.params);
+//     let controller = getController(req.params.controller);
+//     if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//     let action = req.params.action;
+//
+//     let result = await controller[action](req.params.id, req.body);
+//     if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
+//
+//     return res.status(200).send(result);
+//
+// })
+//
+// /**
+//  * view
+//  */
+// router.get('/:controller/:id', async (req, res) => {
+//     let controller = getController(req.params.controller);
+//     if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//
+//     let result = await controller.view(req.params.id);
+//     if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
+//
+//     return res.status(200).send(result);
+// })
+//
+// /**
+//  * add
+//  */
+// router.post('/:controller', async (req, res) => {
+//     let controller = getController(req.params.controller);
+//     if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//
+//     let result = await controller.add(req.body);
+//     if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
+//
+//     return res.status(200).send(result);
+// })
+//
+// async function apiPatchPutHandler(req, res) {
+//     let controller = getController(req.params.controller);
+//     if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//
+//     let result = await controller['edit'](req.params.id, req.body);
+//     if (!result) return res.status(404).send(`The requested ${req.params.controller} could not be found`);
+//
+//     return res.status(200).send(result);
+// }
+//
+// /**
+//  * edit
+//  */
+// router.patch('/:controller/:id', apiPatchPutHandler)
+//
+// /**
+//  * edit
+//  */
+// router.
+// router.put('/:controller/:id',  apiPatchPutHandler)
+//
+// /**
+//  * delete
+//  */
+// router.delete('/:controller/:id', (req, res) => {
+//     let controller = getController(req.params.controller);
+//     if(!controller) return res.status(404).send(`${req.params.controller} could not be found`);
+//
+//     let result = controller.delete(req.params.id);
+//     if (!result) return res.status(404).send(`The requested '${req.params.controller}' record could not be found`);
+//
+//     return res.status(200).send(result);
+// })
 
 module.exports = router;
