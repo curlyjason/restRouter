adecb992c84f82a2dd38b1853e6f15e0fe706826
Jason Tempestini <jason@curlymedia.com>
2023-04-21 16:07:49 -0700

    start rentals table

---
+++ src/model/RentalsTable.js
@@ -0,0 +1,85 @@
+
+require('../utility/Date');
+class RentalsTable extends require('./Table.js') {
+    CustomersTable = require('./CustomersTable');
+    MoviesTable = require('./MoviesTable');
+    joiSchema = this.Joi.object({
+        rentalDate: this.Joi.date().required(),
+        dueDate: this.Joi.date().required(),
+        returned: this.Joi.boolean(),
+        numberInStock: this.Joi.number(),
+        dailyRentalRate: this.Joi.number(),
+        genres: this.Joi.array()
+    });
+
+    schema = new this.mongoose.Schema({
+        rentalDate: {type: Date, default: new Date()},
+        dueDate: {type: Date, default: (new Date()).addDays(3)},
+        returned: {type: Boolean, default: false},
+        price: Number,
+        customer: {
+            type: this.CustomersTable.schema,
+            required: true
+        },
+        movie: {
+            type: this.MoviesTable.schema,
+            required: true
+        }
+    })
+
+    Rentals = this.mongoose.model('rentals', this.schema);
+
+    async find () {
+        let find = async () => {
+            return await this.Rentals.find()
+                .then((result) => {
+                    return result;
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                    return err;
+                })
+        };
+        return await this.connection(find)
+    }
+
+    async findById (id) {
+        let findById = async () => {
+            return await this.Rentals.findById(id)
+                // .populate('genres')
+                // .select('name genres')
+                .then((result) => {
+                    return result;
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                    return err;
+                });
+        }
+        return await this.connection(findById);
+    }
+
+    async save (data) {
+        let save = async () => {
+            let entity = new this.Rentals(data);
+            return await entity.save();
+        }
+        return await this.connection(save);
+    }
+
+    async update (entity, data) {
+        let update = async () => {
+            return await entity.updateOne(data);
+        }
+        return await this.connection(update);
+    }
+
+    async delete (entity) {
+        let update = async () => {
+            return await entity.deleteOne();
+        }
+        return await this.connection(update);
+    }
+}
+
+module.exports = new RentalsTable();
