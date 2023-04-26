25c5557cbfd1c8158f8a83d595ad2a0004d5fe6a
dreamingmind <ddrake@dreamingmind.com>
2023-04-26 13:00:23 -0700

    set inheritance for additional controllers

--- src/controllers/api/CustomersController.js
+++ src/controllers/api/CustomersController.js
@@ -1,52 +1,6 @@
 class CustomersController
 {
 
-    Customers = require('../../model/CustomersTable');
-
-    /**
-     * @returns {[{name: string, id: number},{name: string, id: number}]}
-     */
-    async index() {
-        console.log('waiting...');
-        return await this.Customers.find();
-    }
-
-    /**
-     * @param id
-     * @returns {{name: string, id: number}|{name: string, id: number}} | undefined
-     */
-    async view(id) {
-        return await this.Customers.findById(id);
-    }
-
-    /**
-     * @param {{name}} data
-     * @returns {{name, id: *}}
-     */
-    async add(data) {
-        let { error } = this.Customers.joiSchema.validate(data);
-        if(error) return {
-            error: error.message,
-            post_data: data
-        };
-
-        return await this.Customers.save(data);
-    }
-
-    async edit(id, data) {
-        let entity = await this.Customers.findById(id);
-        return await this.Customers.update(entity, data);
-    }
-
-    async delete(id) {
-        let entity = await this.Customers.findById(id);
-        if(entity){
-            return await this.Customers.delete(entity);
-        }
-        else {
-            return new Error('Record not found');
-        }
-    }
 }
 
-module.exports = new CustomersController();
+module.exports = CustomersController;
