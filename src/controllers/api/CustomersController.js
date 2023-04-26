0e1f46f64b4fb1a7882f5e314f8805e33c88c892
dreamingmind <ddrake@dreamingmind.com>
2023-04-26 13:52:41 -0700

    inherit for remaining controllers

--- src/controllers/api/CustomersController.js
+++ src/controllers/api/CustomersController.js
@@ -1,4 +1,4 @@
-class CustomersController
+class CustomersController extends require('../Controller')
 {
 
 }
--- src/controllers/api/MoviesController.js
+++ src/controllers/api/MoviesController.js
@@ -1,4 +1,4 @@
-class MoviesController
+class MoviesController extends require('../Controller')
 {
 
 }
