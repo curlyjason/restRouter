b0cc0e5adc84a7e2fd252bbd28e36bc9858f6a07
dreamingmind <ddrake@dreamingmind.com>
2023-04-27 13:05:04 -0700

    write table helpers

--- src/views/helpers/HtmlHelper.js
+++ src/views/helpers/HtmlHelper.js
@@ -3,6 +3,32 @@ function p(content) {
     return `<p>${content}</p>`;
 }
 
+function tCells(obj) {
+    let accum = '<tr>';
+    for (let value of Object.values(obj)) {
+        accum += `<td>${value}</td>`;
+    }
+    return accum + '</tr>';
+}
+function tHeaders(obj) {
+    let accum = '<tr>';
+    for (let key of Object.keys(obj)) {
+        accum += `<th>${key}</th>`;
+    }
+    return accum + '</tr>';
+}
 
+function tBody(obj) {
+    return `<tBody>
+        ${this.tHeaders(obj)}
+        ${this.tCells(obj)}
+        </tBody>`;
+}
 
-module.exports = {p};
+function table(obj) {
+    return `<table>
+        ${this.tBody(obj)}
+    </table>`;
+}
+
+module.exports = {p, tBody, tCells, tHeaders, table};
