4399b7244f00ab29968672815271d962c5aeb529
dreamingmind <ddrake@dreamingmind.com>
2023-04-27 11:33:18 -0700

    write an example csv output page

--- src/controllers/GenresController.js
+++ src/controllers/GenresController.js
@@ -2,6 +2,10 @@
 
 class GenresController extends require('./Controller') {
 
+    async index() {
+        this.set({genres: super.index()});
+        return this.View.render();
+    }
 }
 
 module.exports = GenresController
