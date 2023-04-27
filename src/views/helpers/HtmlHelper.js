e98689e44317e49fc516375593066cf8e4333232
dreamingmind <ddrake@dreamingmind.com>
2023-04-27 14:29:55 -0700

    adjust table methods to handle key lists

--- src/views/helpers/HtmlHelper.js
+++ src/views/helpers/HtmlHelper.js
@@ -3,31 +3,36 @@ function p(content) {
     return `<p>${content}</p>`;
 }
 
-function tCells(obj) {
+function tCells(obj, keys) {
+    // return `<tr><td>${obj.name}</td></tr>`;
     let accum = '<tr>';
-    for (let value of Object.values(obj)) {
-        accum += `<td>${value}</td>`;
+
+    for (let key of keys) {
+        accum += `<td>${obj[key]}</td>`;
     }
+
     return accum + '</tr>';
 }
-function tHeaders(obj) {
+function tHeaders(keys) {
+    // return `<!--<tr><th>'name'</th></tr>-->`;
     let accum = '<tr>';
-    for (let key of Object.keys(obj)) {
+    for (let key of keys) {
         accum += `<th>${key}</th>`;
     }
     return accum + '</tr>';
 }
 
-function tBody(obj) {
+function tBody(obj, keys) {
     return `<tBody>
-        ${this.tHeaders(obj)}
-        ${this.tCells(obj)}
+        ${this.tHeaders(keys)}
+        ${this.tCells(obj, keys)}
         </tBody>`;
 }
 
-function table(obj) {
+function table(obj, keys) {
+    // console.log(keys)
     return `<table>
-        ${this.tBody(obj)}
+        ${this.tBody(obj, keys)}
     </table>`;
 }
 
