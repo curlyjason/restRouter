9116d2cb5bd695c055dfd5e2d62a35cafebf9448
dreamingmind <ddrake@dreamingmind.com>
2023-04-28 14:42:21 -0700

    revise edit for output

--- src/views/templates/Genres/edit.js
+++ src/views/templates/Genres/edit.js
@@ -6,7 +6,7 @@ async function run() {
     let genre = await this.genre;
     let accum = '';
 
-    accum += Form.create({}, `${genre._id}`, {method: 'patch'});
+    accum += Form.create(genre, `/genres/edit/${genre._id}`, {method: 'PATCH'});
     accum += Form.submit();
     accum += Form.end();
 
