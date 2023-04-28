cd850c638a35c9063ca367791d0a3682a3daf95e
dreamingmind <ddrake@dreamingmind.com>
2023-04-28 08:28:00 -0700

    add view links to index tables

--- src/views/templates/Genres/index.js
+++ src/views/templates/Genres/index.js
@@ -3,17 +3,25 @@ const Html = require('../../helpers/HtmlHelper');
 
 async function run() {
     let genres = await this.genres;
-    let accum = `<table>
-        <tbody>
-            ${Html.tHeaders(this.allowedKeys)}`;
+    let accum = `
+<table>
+    <tbody>
+        ${Html.tHeaders(this.allowedKeys)}`;
 
-    for(let i = 0; i < genres.length; i++) {
-        accum += Html.tCells(genres[i], this.allowedKeys);
-    }
+        for(let i = 0; i < genres.length; i++) {
+            accum += Html.tCells(
+                genres[i],
+                this.allowedKeys,
+                {
+                    _id: (obj) => {return Html.link(obj._id, `view/${obj._id}`)}
+                }
+            );
+        }
 
     return accum + `
     </tbody>
-</table>`;
+</table>
+`;
 }
 
 module.exports.run = run;
