19e16c820977095125be10122e9fb15925fae2ec
dreamingmind <ddrake@dreamingmind.com>
2023-04-28 09:03:37 -0700

    add links to view page

--- src/views/templates/Genres/view.js
+++ src/views/templates/Genres/view.js
@@ -1,8 +1,39 @@
 const Html = require('../../helpers/HtmlHelper');
 
+function getIndexLink(genre) {
+    return Html.p(
+        Html.link('Index', '/genres/')
+    )
+}
+
+function getAddLink(genre) {
+    return Html.p(
+        Html.link('New', 'add/')
+    )
+}
+
+function getEditLink(genre) {
+    return Html.p(
+        Html.link('Edit', `edit/${genre._id}`)
+    )
+}
+
+function getDeleteLink(genre) {
+    return Html.p(
+        Html.link('Delete', `edit/${genre._id}`)
+    )
+}
+
 async function run() {
     let genre = await this.genre;
-    let accum = '<dl>';
+    let accum = '';
+
+    accum += getIndexLink(genre);
+    accum += getAddLink(genre);
+    accum += getEditLink(genre);
+    accum += getDeleteLink(genre);
+
+    accum += '<dl>';
 
     for(let i = 0; i < this.allowedKeys.length; i++) {
         accum += `<dt>${this.allowedKeys[i]}</dt>
