9752c95d0b4c598bfd45e8fdb0a7cfe78f470d04
Jason Tempestini <jason@curlymedia.com>
2023-05-04 14:54:38 -0700

    install and setup Fawn for transactions

--- src/model/Table.js
+++ src/model/Table.js
@@ -2,11 +2,16 @@
 class Table {
 
     mongoose = require("mongoose");
+    Fawn = require("fawn");
     mongo = this.mongoose.mongo;
 
     database = 'vidly';
 
     Joi = require('joi');
+
+    constructor() {
+        this.Fawn.init(this.mongoose);
+    }
 
     async connection(callback) {
         return this.mongoose.connect(`mongodb://db:27017/${this.database}`, {
