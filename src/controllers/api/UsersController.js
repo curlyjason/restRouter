7990db18751be4fe64d874690e7dbce068b53c94
Jason Tempestini <jason@curlymedia.com>
2023-05-11 14:50:50 -0700

    override add with existance validation

--- src/controllers/api/UsersController.js
+++ src/controllers/api/UsersController.js
@@ -1,5 +1,27 @@
 class UsersController extends require('../Controller')
 {
+    async add(data) {
+        const User = this.defaultTable();
+        let { error } = User.joiSchema.validate(data);
+
+        if(error) return {
+            error: error.message,
+            post_data: data
+        };
+
+        console.log(data.email);
+
+        let user = await User.find( {"email" : data.email});
+
+        if(user.length > 0){
+            return {
+                error: "User already exists.",
+                post_data: data
+            };
+        }
+
+        return await User.save(data);
+    }
 
 }
 
